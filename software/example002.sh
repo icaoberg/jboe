08e8519de06202c068ab2489a7285df665f2856c
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2017-02-17 01:05:17 -0500

    Moved files

---
+++ software/example002.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+which matlab
+module load matlab-8.1
+which matlab
---
+++ software/example003.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+which python
+which virtualenv
+
+module load python27
+module load python27-extras
+
+which python
+which virtualenv
